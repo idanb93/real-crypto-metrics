d9ef63763bb53afa0b3b92a624c1a5a406e6c2bd
idanb93 <idanbar93@gmail.com>
2022-09-11 17:51:43 +0300

    commented out prisma in the backend

--- backend/src/sqlConnection/client.ts
+++ backend/src/sqlConnection/client.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasources: { db: { url: process.env.POSTGRESQL_URL } },
-});
+// const prisma = new PrismaClient({
+//   datasources: { db: { url: process.env.POSTGRESQL_URL } },
+// });
 
-export default prisma;
+// export default prisma;
