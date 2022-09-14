49d0ec35fc20fbf846d4cfdd6bc7558079f2ba88
idanb93 <idanbar93@gmail.com>
2022-09-14 13:14:22 +0300

    configured ESLint and Prettier to work with TypeScript and added initial Winston Logger configurations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,28 @@
-import express, { Express } from "express";
-import dotenv from "dotenv";
-import { resolve } from "path";
-// import { connectPostgreSqlDB } from "./services/sqlConnector";
+import express, { Express } from 'express'
+import dotenv from 'dotenv'
+import { resolve } from 'path'
+// import { connectPostgreSqlDB } from "./services/sqlConnector"
+import { logger } from './logger/index'
 
-dotenv.config({ path: resolve(__dirname, "../.env") });
+dotenv.config({ path: resolve(__dirname, '../.env') })
 
-const isValidPort = () => {
-    const portAsNumber = Number(process.env.PORT);
-    return !isNaN(portAsNumber) && portAsNumber >= 0 && portAsNumber < 65536;
+const isValidPort = (): boolean => {
+  const portAsNumber = Number(process.env.PORT)
+  return !isNaN(portAsNumber) && portAsNumber >= 0 && portAsNumber < 65536
 }
 
-const port: number = isValidPort() ? Number(process.env.PORT) : 3000;
-const app: Express = express();
+const port: number = isValidPort() ? Number(process.env.PORT) : 3000
+const app: Express = express()
 
 // if (!process.env.POSTGRESQL_URL) {
 //   console.error(
 //     '[error]: The "POSTGRESQL_URL" environment variable is required'
 //   );
-//   process.exit(1);
+//   process.exit(1)
 // }
 
 // async function connectPostgreSql() {
-//   const { POSTGRESQL_URL = null } = process.env;
+//   const { POSTGRESQL_URL = null } = process.env
 
 //   if (!POSTGRESQL_URL) {
 //     console.error(
@@ -29,19 +30,19 @@ const app: Express = express();
 //     );
 //     process.exit(1);
 //   }
-//   await connectPostgreSqlDB();
+//   await connectPostgreSqlDB()
 // }
 
 // (async () => {
-//   app.use(helmet());
-//   app.use(cors());
-//   app.use(bodyParser.json({ limit: "5mb" }));
-//   app.use(bodyParser.urlencoded({ extended: true }));
+//   app.use(helmet())
+//   app.use(cors())
+//   app.use(bodyParser.json({ limit: "5mb" }))
+//   app.use(bodyParser.urlencoded({ extended: true }))
 
 //   configureRoutes(app);
 
-//   await connectPostgreSql();
-//   app.listen(port, () => console.log(`Running on ${port}`));
+//   await connectPostgreSql()
+//   app.listen(port, () => console.log(`Running on ${port}`))
 // })();
 
-app.listen(port, () => console.log(`Running on ${port}`));
+app.listen(port, () => logger.info(`Running on ${port}`))
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,22 +1,23 @@
-import { Application } from "express";
+// import { logger } from '../logger/index'
+// import { Application } from 'express'
 // import HealthRouter from './Health';
 // import FiltersRouter from './Filters';
 
-const Routes = [
-  //   { path: '/', router: FiltersRouter},
-  //   { path: '/health', router: HealthRouter },
-];
+// const Routes = [
+//     { path: '/', router: FiltersRouter},
+//     { path: '/health', router: HealthRouter },
+// ]
 
-const configureRoutes = (app: Application) => {
-  if (!app || !app.use) {
-    console.error(
-      "[Error] Route Initialization Failed: app / app.use is undefined"
-    );
-    return process.exit(1);
-  }
+// const configureRoutes = (app: Application): void => {
+//   if (!app || !app.use) {
+//     logger.error(
+//       '[Error] Route Initialization Failed: app / app.use is undefined'
+//     )
+//     return process.exit(1)
+//   }
 
-  // Custom Routes
-  //   Routes.forEach((route) => app.use(route.path, route.router));
-};
+//   // Custom Routes
+//     Routes.forEach((route) => app.use(route.path, route.router));
+// }
 
-export { configureRoutes };
+// export { configureRoutes }
