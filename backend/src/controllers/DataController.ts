65561704bdf6e8bf53f9cfdb943b9bb484239974
idanb93 <idanbar93@gmail.com>
2022-09-26 10:47:00 +0300

    changed getting data function from async to regular function

--- backend/src/controllers/DataController.ts
+++ backend/src/controllers/DataController.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Route, SuccessResponse } from 'tsoa'
 import { Project } from '../interfaces/frontend'
-import { logger } from '../logger'
 import { _getInitialData } from '../services/data'
 
 @Route('/data')
@@ -8,13 +7,8 @@ export class DataController extends Controller {
   @SuccessResponse('200', 'OK') // Custom success response
   @Get('/initial')
   // eslint-disable-next-line @typescript-eslint/space-before-function-paren
-  public async getInitialData(): Promise<Project[]> {
-    try {
-      const result: Project[] = _getInitialData()
-      return result ?? []
-    } catch (err) {
-      logger.info('unable to get projects contributors')
-      throw err
-    }
+  public getInitialData(): Project[] {
+    const result = _getInitialData()
+    return result
   }
 }
